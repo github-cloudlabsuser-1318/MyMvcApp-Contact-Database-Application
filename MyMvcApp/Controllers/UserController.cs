7643ad7c8b62e08dbddb1cc0944125d9bb497926
iwyu02ZYF*sP <github_cloudlabsuser_1318@cloudlabsaiuser.com>
2025-05-23 13:16:51 +0000

    Added search functionality

---
+++ MyMvcApp/Controllers/UserController.cs
@@ -0,0 +1,111 @@
+using Microsoft.AspNetCore.Mvc;
+
+using MyMvcApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyMvcApp.Controllers
+{
+    public class UserController : Controller
+    {
+        public static List<User> userlist = new List<User>();
+
+        // GET: User
+        public IActionResult Index()
+        {
+            return View(userlist);
+        }
+
+        // GET: User/Details/5
+        public IActionResult Details(int id)
+        {
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+                return NotFound();
+            return View(user);
+        }
+
+        // GET: User/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                user.Id = userlist.Count > 0 ? userlist.Max(u => u.Id) + 1 : 1;
+                userlist.Add(user);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        // GET: User/Edit/5
+        public IActionResult Edit(int id)
+        {
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+                return NotFound();
+            return View(user);
+        }
+
+        // POST: User/Edit/5
+        [HttpPost]
+        public IActionResult Edit(int id, User user)
+        {
+            var existingUser = userlist.FirstOrDefault(u => u.Id == id);
+            if (existingUser == null)
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                existingUser.Name = user.Name;
+                existingUser.Email = user.Email;
+                // Update other properties as needed
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        // GET: User/Delete/5
+        public IActionResult Delete(int id)
+        {
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+                return NotFound();
+            return View(user);
+        }
+
+        // GET: User/Search
+        public ActionResult Search(string query)
+        {
+            if (string.IsNullOrWhiteSpace(query))
+            {
+                return View("Index", userlist);
+            }
+ 
+            var results = userlist
+                .Where(u =>
+                    (!string.IsNullOrEmpty(u.Name) && u.Name.Contains(query, StringComparison.OrdinalIgnoreCase)) ||
+                    (!string.IsNullOrEmpty(u.Email) && u.Email.Contains(query, StringComparison.OrdinalIgnoreCase))
+                )
+                .ToList();
+ 
+            return View("Index", results);
+        }   
+        [HttpPost]
+        public IActionResult Delete(int id, Microsoft.AspNetCore.Http.IFormCollection collection)
+        {
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+                return NotFound();
+
+            userlist.Remove(user);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
